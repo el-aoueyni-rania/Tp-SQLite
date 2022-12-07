05119cf1c820a628951c9c469a63b9a784e12e64
el-aoueyni-rania <elaoueynirania@gmail.com>
2022-12-07 21:14:54 +0100

    first commit

---
+++ app/src/main/java/com/example/tpsqlite/DBContract.kt
@@ -0,0 +1,19 @@
+package com.example.tpsqlite
+
+import android.provider.BaseColumns
+
+object DBContract {
+    class EtudiantEntry : BaseColumns {
+        companion object {
+            val TABLE_NAME = "etudiant"
+            val COLUMN_ETUDIANT_ID = "id"
+            val COLUMN_NAME_NOM = "nom"
+            val COLUMN_NAME_PRENOM = "prenom"
+            val COLUMN_NAME_PHONE = "phone"
+            val COLUMN_NAME_EMAIL = "email"
+            val COLUMN_NAME_LOGIN = "login"
+            val COLUMN_NAME_MDP = "mdp"
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tpsqlite/EtudiantModel.kt
@@ -0,0 +1,8 @@
+package com.example.tpsqlite
+
+data class EtudiantModel (val nom: String,
+                          val prenom: String,
+                          val phone: String,
+                          val email: String,
+                          val login: String,
+                          val password: String)
